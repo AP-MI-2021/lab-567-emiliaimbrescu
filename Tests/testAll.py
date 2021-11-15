2d1eeee27adec39783af64337f70398dc6eed5df
emiliaimbrescu <91563558+emiliaimbrescu@users.noreply.github.com>
2021-11-15 17:24:06 +0200

    Am apelat testele Undo si Redo

--- Tests/testAll.py
+++ Tests/testAll.py
@@ -1,4 +1,5 @@
 from Tests.testCRUD import test_adauga_vanzare_carte, test_sterge_carte
+from Tests.testUndoRedo import test_undo_redo
 from Tests.test_functionalitati import test_aplicare_discount, test_modifica_genul, test_pret_minim, \
     test_ordine_crescatoare, test_titluri_distincte
 from Tests.testdomain import testcarte
@@ -13,3 +14,6 @@ def run_all_tests():
     test_pret_minim()
     test_ordine_crescatoare()
     test_titluri_distincte()
+    test_undo_redo()
+    print("Salut!")
+
